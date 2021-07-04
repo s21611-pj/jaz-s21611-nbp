37856eec4a438a78e774d655ee3449d2fced883c
tursonsko <turekwojtek@gmail.com>
2021-07-04 16:39:26 +0200

    Initial commit

---
+++ src/main/java/com/example/jazs21611nbp/JazS21611NbpApplication.java
@@ -0,0 +1,13 @@
+package com.example.jazs21611nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS21611NbpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JazS21611NbpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jazs21611nbp/JazS21611NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jazs21611nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazS21611NbpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
